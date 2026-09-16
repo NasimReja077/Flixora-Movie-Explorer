e9b1e4b48c3fb3e7722c2a0b19348dd3854111f7
Nasim <nasim.reja.077@gmail.com>
2026-09-17 00:00:01 +0530

    feat: add movies reducer to Redux store configuration

--- Frontend/src/app/app.store.js
+++ Frontend/src/app/app.store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/state/auth.slice.js";
+import moviesReducer from "../features/movies/store/movies.slice.js";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    movies: moviesReducer,
   },
 });
 
